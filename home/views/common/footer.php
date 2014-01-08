87bc9462dd28b11076f4d801b97689a16c58ed47
robertpen <robertpen@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-01-08 07:25:21 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@17 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/views/common/footer.php
+++ home/views/common/footer.php
@@ -19,8 +19,7 @@
         	<a href="http://www.openol.com/" target="_blank">渠道中国 </a>|
         	<a href="http://www.openol.com/" target="_blank">渠道中国 </a>|
         	<a href="http://www.78.cn/" target="_blank">致富网</a>|
-        	<a href="http://www.openol.com/" target="_blank">渠道中国 </a>|
-        	<a href="#">渠道中国 </a>
+        	<a href="http://www.openol.com/" target="_blank">渠道中国 </a>
         </div>
         <p class="sponsor"><strong>© 7808创业指导网 荣誉出品</strong> </p>
         <div class="foot_nav"><a href="#">关于我们</a>|<a href="#">广告合作</a>|<a href="#">联系我们</a>|<a href="#">版权声明</a>|<a href="#">站长统计</a></div>
--- home/views/common/header.php
+++ home/views/common/header.php
@@ -2,7 +2,14 @@
     <div class="top_inner">
         <div class="logo"><a href="#"><img src="<?php echo src_url('images/logo.jpg'); ?>" width="242" height="41"></a></div>
         <div class="search_box">
-            <p><strong>热门搜索 :</strong> <a href="#">问题</a><a href="#">服装</a><a href="#">建材</a><a href="#">美食</a><a href="#">服装</a></p>
+            <p>
+            	<strong>热门搜索 :</strong> 
+            	<a href="#">问题</a>
+            	<a href="#">服装</a>
+            	<a href="#">建材</a>
+            	<a href="#">美食</a>
+            	<a href="#">服装</a>
+            </p>
             <form>
                 <input name="" type="text" class="search_text">
                 <input name="" type="button" class="search_btn" value="搜索">
