a8a3bbacd16596a4c05ba06790504a6fdd292dde
robertpen <robertpen@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-01-09 06:58:23 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@36 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/views/question/question_add.php
+++ home/views/question/question_add.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title><?php if (isset($meta['title'])) echo $meta['title']; ?>中国创业指导网</title>
-    <meta name="keywords" content="<?php if (isset($meta['keywords'])) echo $meta['keywords']; ?>"/>
-    <meta name="description" content="<?php if (isset($meta['description'])) echo $meta['description']; ?>"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo src_url('css/style.css'); ?>"/>
-</head>
-<body>
-<?php $this->load->view('common/header.php'); ?>
-
 <div class="content">
     <div class="content_question">
         <h1 class="issue">发布一个新问题</h1>
@@ -37,7 +25,3 @@
         </div>
     </div>
 </div>
-
-<?php $this->load->view('common/footer.php'); ?>
-</body>
-</html>
--- home/views/user/user_center.php
+++ home/views/user/user_center.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title><?php if (isset($meta['title'])) echo $meta['title']; ?></title>
-    <meta name="keywords" content="<?php if (isset($meta['keywords'])) echo $meta['keywords']; ?>"/>
-    <meta name="description" content="<?php if (isset($meta['description'])) echo $meta['description']; ?>"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo src_url('css/style.css'); ?>"/>
-</head>
-<body>
-<?php $this->load->view('common/header.php'); ?>
-
-<?php $this->load->view('common/footer.php'); ?>
-</body>
-</html>
+用户中心
