ad0cbbcc1372c1baa31957e123b7ebb04a362d87
robertpen <robertpen@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-01-17 02:21:28 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@84 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/controllers/user.php
+++ home/controllers/user.php
@@ -71,24 +71,25 @@ class User extends HM_Controller
     {
     	if ($_POST) {
     		$user 		= $this->input->post('username');
+    		$nickname   = $this->input->post('nickname');
     		$password 	= $this->input->post('password');
     		$repassword = $this->input->post('repassword');
     		
-    		if (!$user) {
-    			$data = array('info' => '用户名不能为空!', 'status' => '0');
-    		}
-    		
-    		if (!$password) {
-    			$data = array('info' => '密码不能为空!', 'status' => '0');
-    		}
-    		
-    		if (!$repassword) {
-    			$data = array('info' => '重复密码不能为空!', 'status' => '0');
-    		}
-    		
-    		if ($password !== $repassword) {
-    			$data = array('info' => '两次输入的密码不一致!请重新输入', 'status' => '0');
-    		}
+//    		if (!$user) {
+//    			$data = array('info' => '用户名不能为空!', 'status' => '0');
+//    		}
+//    		
+//    		if (!$password) {
+//    			$data = array('info' => '密码不能为空!', 'status' => '0');
+//    		}
+//    		
+//    		if (!$repassword) {
+//    			$data = array('info' => '重复密码不能为空!', 'status' => '0');
+//    		}
+//    		
+//    		if ($password !== $repassword) {
+//    			$data = array('info' => '两次输入的密码不一致!请重新输入', 'status' => '0');
+//    		}
     	
     		$this->load->model('User_model');
     		$res = $this->User_model->verify_login($user, $password);
