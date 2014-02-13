093e8d3e86194903d8f0b95e5dfe66e77fc77e2a
shangweidan <shangweidan@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-02-13 05:46:45 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@121 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/views/changecity.php
+++ home/views/changecity.php
@@ -5,11 +5,10 @@
     <title>中国创业指导网</title>
     <link rel="stylesheet" type="text/css" href="<?php echo src_url('css/style.css'); ?>"/>
 </head>
-<body>
+<body class="citylist_box">
 <div class="header">
     <div class="head_box">
         <p class="left">你好，欢迎来到中国创业指导网！</p>
-
         <div class="right">
             <span><a href="#">请登录</a></span><span><a href="#">免费注册</a></span><span><a href="#">指导网首页</a></span><span
                 class="mobile"><a href="#">手机版</a></span>
@@ -17,16 +16,12 @@
     </div>
 </div>
 <div class="second_top">
-    <div class="logo2"><a href="#"><img width="199" height="36"
-                                        src="http://www.7808.com/static/images/logo_two.jpg"></a></div>
+    <div class="logo2"><a href="#"><img width="199" height="36" src="http://www.7808.com/static/images/logo_two.jpg"></a></div>
     <div class="right">
         <p><a href="#" class="current">找产品</a><a href="#">找求购</a><a href="#">找公司</a></P>
-
         <div class="input_box">
             <form>
-                <input type="text" name="" class="text_search"> <input type="button" name="" class="btn_search"
-                                                                       value="搜索"> <strong class="post_message"><a
-                        href="#">免费发布信息</a></strong>
+                <input type="text" name="" class="text_search"> <input type="button" name="" class="btn_search" value="搜索"> <strong class="post_message"><a href="#">免费发布信息</a></strong>
             </form>
         </div>
     </div>
@@ -41,13 +36,42 @@
         <li class="no_bg"><a href="#">企业名录</a></li>
     </ul>
 </div>
-
-
-<h1>城市列表  --  页面</h1>
-
-
-<div class="text_city">
-    中国创业网中国免费发布信息平台、优秀B2B电子商务网站，汇集海量免费企业黄页信息、供求信息，企业可以免费发布信息、免费发布供求信息、免费发布求购信息。黄页88免费发布信息网是帮助企业网络销售产品、免费发布信息、免费刊登黄页、免费网站推广的首选免费信息发布平台，而且各地区有分站，推荐北京免费发布信息、上海免费发布信息、广州免费发布信息。
+<div class="cityinner_box">
+		<div class="entrance">
+			<h3><a href="#">进入北京站》</a></h3><span>省级免费发布信息网</span><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a><a href="#">北京</a><a href="#">上海</a><a href="#">广东</a><a href="#">北京</a><a href="#">上海</a>
+		</div>	
+		<div class="city_select">
+			<p>按省份选择城市：</p><form><div class="city_search"><select value=""><option></option><option>重庆</option></select></div><input type="button" name="" class="city_btn" value=""></form>
+		</div>
+		<ul class="city_sort">
+			<li><span>A</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>B</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>C</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>D</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>E</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>F</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>G</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>H</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>I</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>J</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>K</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>L</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>M</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>N</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>O</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>P</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>Q</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>R</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>S</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>T</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>U</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>V</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>W</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>X</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>Y</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a></p></li>
+			<li><span>Z</span><P><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a><a href="#">安庆</a><a href="#">安阳</a></p></li>																																																																											
+		</ul>
+		<p class="copyright">©2009-2014  中国创业指导网所有</P>
 </div>
 </body>
 </html>
