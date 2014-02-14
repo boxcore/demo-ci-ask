ab8c0fc6e58485c0c247d39b83fa782d03a6bdee
shangweidan <shangweidan@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-02-14 03:22:34 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@128 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/views/city_list.php
+++ home/views/city_list.php
@@ -5,7 +5,7 @@
     <title>中国创业指导网</title>
     <link rel="stylesheet" type="text/css" href="<?php echo src_url('css/style.css'); ?>"/>
 </head>
-<body class="citylist_box">
+<body>
 <div class="header">
     <div class="head_box">
         <p class="left">你好，欢迎来到中国创业指导网！</p>
@@ -23,12 +23,12 @@
             <form>
                 <input type="text" name="" class="text_search"> <input type="button" name="" class="btn_search" value="搜索"> <strong class="post_message"><a href="#">免费发布信息</a></strong>
             </form>
-        </div>
+        </div>                   
     </div>
 </div>
+<div class="crumbs">当前位置：<a href="#">首页</a> &gt;<span class="current">餐饮食品加盟</span></div>
 
-<h1>城市列表页面</h1>
 
-<div class="text_city">中国创业网中国免费发布信息平台、优秀B2B电子商务网站，汇集海量免费企业黄页信息、供求信息，企业可以免费发布信息、免费发布供求信息、免费发布求购信息。黄页88免费发布信息网是帮助企业网络销售产品、免费发布信息、免费刊登黄页、免费网站推广的首选免费信息发布平台，而且各地区有分站，推荐北京免费发布信息、上海免费发布信息、广州免费发布信息。</div>
+
 </body>
 </html>
