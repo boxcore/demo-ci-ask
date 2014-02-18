6a4401a2020c7099b748465aeee1e6369624b816
shangweidan <shangweidan@847f1278-3494-4994-8dfb-0e8c01426fa5>
2014-02-18 06:52:38 +0000

    git-svn-id: http://www.svnchina.com/svn/xinwenda@140 847f1278-3494-4994-8dfb-0e8c01426fa5

--- home/views/info.php
+++ home/views/info.php
@@ -1,1 +1,11 @@
-<h1>消息提醒</h1>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>中国创业知道网</title>
+<link rel="stylesheet" type="text/css" href="<?php echo src_url('css/style.css');?>"/>
+</head>
+<body class="have_bg">
+	<h1 class="string_message">N秒后将跳转到<a href="#">首页</a></h1>
+</body>
+</html>
